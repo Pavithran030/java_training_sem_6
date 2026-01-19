cd5ebb9d8b908d48538c47ab7766e05cadaff055
Pavithran030 <techpavithran18@gmail.com>
2026-01-19 23:58:59 +0530

    Practice Programs

--- Day1_19-01-2026/KaprekarNumber.java
+++ Day1_19-01-2026/KaprekarNumber.java
@@ -17,7 +17,7 @@ public class KaprekarNumber {
 
             int norco = 0;
             while (temp > 0) {
-                int digit = temp % 10;
+
                 temp /= 10;
                 norco++;
             }
