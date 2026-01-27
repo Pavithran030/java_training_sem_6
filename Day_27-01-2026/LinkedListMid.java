f1ec748906d36a5b84d1c34bbc794789623b35e1
Pavithran030 <techpavithran18@gmail.com>
2026-01-27 16:13:00 +0530

    DSA Basics

---
+++ Day_27-01-2026/LinkedListMid.java
@@ -0,0 +1,123 @@
+
+class Node {
+
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+}
+
+class Linkedlist {
+
+    Node head;
+    int count = 0, evencount = 0;
+
+    void insert(int a) {
+
+        Node newNode = new Node(a);
+        newNode.next = null;
+        /*
+
+        Way --1
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+         */
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+
+    }
+
+    // Count the elements in the Linkedlist
+    void count() {
+        Node tem1 = head;
+
+        while (tem1 != null) {
+            count++;
+            int num = tem1.data;
+
+            if (num % 2 == 0) {
+                evencount++;
+            }
+            tem1 = tem1.next;
+        }
+    }
+
+    // Display all the linked list
+    void displayele(Node temp) {
+
+        while (temp != null) {
+            if (temp.next == null) {
+                System.out.print(temp.data);
+            } else {
+                System.out.print(temp.data + " -> ");
+            }
+            temp = temp.next;
+        }
+    }
+
+    // Print the Middle element in the List
+    void middle() {
+        Node temp = head;
+
+        displayele(temp);
+        count();
+
+        int mid = count / 2;
+        if (count % 2 != 0) {
+            mid = mid + 1;
+        }
+
+        // mid-1 => everytime looping the node point to the next element.so i stop the loop before 1 step.
+        for (int i = 0; i < mid - 1; i++) {
+            temp = temp.next;
+        }
+        System.out.println("\nMid Element : " + temp.data);
+        System.out.println("Event Numbers in the LinkedList : " + evencount);
+    }
+
+    //Delete begin Elements...
+    void deletebegin() {
+        Node tem = head.next;
+        displayele(tem);
+
+    }
+
+}
+
+public class LinkedListMid {
+
+    public static void main(String[] args) {
+        Linkedlist ls = new Linkedlist();
+        ls.insert(1);
+        ls.insert(2);
+        ls.insert(3);
+        ls.insert(4);
+        ls.insert(5);
+        ls.insert(6);
+
+        ls.middle();
+        ls.deletebegin();
+    }
+
+}
---
+++ Day_27-01-2026/Stack.java
@@ -0,0 +1,84 @@
+
+class StackNode {
+
+    int data;
+    StackNode next;
+
+    StackNode(int data) {
+        this.data = data;
+    }
+
+}
+
+class StackCreation {
+
+    StackNode top;
+    int count = 0;
+
+    void push(int data) {
+        StackNode node = new StackNode(data);
+        node.next = top;
+        top = node;
+
+    }
+
+    void pop() {
+        StackNode temp = top;
+        if (top == null) {
+            System.out.println("Stack Undeflow..!!!");
+            return;
+        }
+
+        top = top.next;
+        System.out.print("Remaining Stack : ");
+        display(temp);
+    }
+
+    void revserstack() {
+
+        StackNode curr = top, pre = null, nex;
+
+        while (curr != null) {
+            nex = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = nex;
+        }
+        top = pre;
+
+        display(top);
+    }
+
+    void display(StackNode top) {
+        StackNode temp = top;
+        while (temp != null) {
+            count++;
+            if (temp.next == null) {
+                System.out.print(temp.data);
+            } else {
+                System.out.print(temp.data + " -> ");
+            }
+            temp = temp.next;
+        }
+    }
+}
+
+public class Stack {
+
+    public static void main(String[] args) {
+        StackCreation st = new StackCreation();
+
+        st.push(2);
+        st.push(4);
+        st.push(6);
+        System.out.print("Current Stack Elements : ");
+        st.display(st.top);
+        System.out.println("Total Element in the Stack : " + st.count);
+        System.out.println("Poped Element from the stack : " + st.top.data);
+        st.pop();
+        System.out.print("\nReversed Stack : ");
+        st.revserstack();
+
+    }
+
+}
